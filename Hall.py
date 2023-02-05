1fc1462b614cd18dec576ee36e3c363019d19d05
ehsan hosseini <ehosseini8001@gmail.com>
2023-02-05 07:42:58 +0330

    Hall functions added

--- Hall.py
+++ Hall.py
@@ -5,3 +5,9 @@ class Hall:
     def __init__(self, neighbors: list['Hall'], preferences: list[int]) -> None:
         self.neighbors = neighbors
         self.preferences = preferences
+        
+    def addNeighbor(self, hall: int) -> None:
+        self.neighbors.append(hall)
+
+    def addPreference(self, group: int) -> None:
+        self.preferences.append(group)
