d4450b0c303a47dd934285e45b2e6826282b8c1b
ehsan hosseini <ehosseini8001@gmail.com>
2023-02-05 08:08:30 +0330

    Ac3 added

--- src/Algorithms.py
+++ src/Algorithms.py
@@ -8,6 +8,21 @@ def forward_checking(problem: CSP, hall_index: int, group_index: int) -> None:
             problem.halls[neighbor_index].preferences.remove(group_index)
 
 
+def AC3(problem: CSP , conflicts:list=None) -> bool:
+    if conflicts == None:
+        conflicts = problem.edges
+
+    while conflicts:
+        (hall_one,hall_two)= conflicts.pop(0)
+        if set_uncommon_preferences(hall_one, hall_two):
+            if len(hall_one.preferences) == 0: 
+                return False
+            for neighbor_index in hall_one.neighbors:
+                neighbor_hall = problem.halls[neighbor_index]
+                if (neighbor_hall, hall_one) not in conflicts:
+                    conflicts.append((neighbor_hall, hall_one))
+                    
+    return True
 
  
 def backtracking(problem: CSP) -> CSP | None:
@@ -25,7 +40,9 @@ def backtracking(problem: CSP) -> CSP | None:
         if hall != None:
             forward_checking(state, hall, group)
             
-       
+        else:
+            if not AC3(state):
+                return None
         hall = MRV(state)
         for group in LCV(state, hall)[-1::-1]:
             if not has_conflict(state, hall, group):
