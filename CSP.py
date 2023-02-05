a7de37bfe8032e048918b691cf264077730a1d7e
ehsan hosseini <ehosseini8001@gmail.com>
2023-02-05 08:03:51 +0330

    old files deleted

--- CSP.py
+++
@@ -1,7 +0,0 @@
-from Hall import Hall
-
-class CSP:
-    def __init__(self, halls: list['Hall'], assigned_halls: list[bool], edges: list[tuple['Hall', 'Hall']]) -> None:
-        self.halls = halls
-        self.assigned_halls = assigned_halls
-        self.edges = edges
